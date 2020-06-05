a9bf4af53776a2c5f3230c8b97414fbad428a0a7
dyslexda <50207663+dyslexda@users.noreply.github.com>
2020-06-05 11:21:14 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 GSHEET_CRED = "client_secret.json"
-LOG_KEY = "1jhslwpjOhqU_M-kJd3WuAXb7-AhNzPtCyxe8UBiKcN8"
+LOG_KEY = ''
 MCOH_SHEET = 'MCOH'
-CATCHER_KEY = '1nGmJCP_JGM1Y2sileUmtIYVJs3r9jqDsJS9TL1YDauk'
+CATCHER_KEY = ''
 C_SCOUT_SHEET = 'Scout Sheet'
-PITCHER_KEY = '1eix3BUNeo4AHu-6Z6UvjUYKNsNQI778uNrb1OFaro-0'
+PITCHER_KEY = ''
 P_SCOUT_SHEET = 'Scout Sheet'
